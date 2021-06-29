283770f4afbdf10a3eff3d0a0b2d2a88d959f7a2
Blake Peck <s210561@students.aie.edu.au>
2021-06-29 16:26:11 +1000

    got the insert working after finding out that in the treenode header there were three lines of code i had to fill in that i did not know

--- CDDS_BinaryTree/main.cpp
+++ CDDS_BinaryTree/main.cpp
@@ -73,7 +73,7 @@ int main(int argc, char* argv[])
         {
             // Implement the code to insert valueBoxValue into your binary tree here! 
             m_binaryTree.Insert(valueBoxValue);
-            m_selectedNode = m_binaryTree.Find(valueBoxValue);
+           /* m_selectedNode = m_binaryTree.Find(valueBoxValue);*/
         }
 
         if (GuiButton(Rectangle{ 160, 60, 125, 30 }, GuiIconText(RICON_CROSS, "Remove")))
